13fb5457e5f90beb22f385a2e1299aed6022bef2
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-09-12 17:58:00 +0300

    update config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     '''
     SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     ARTICLE_SOURCE_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&pageSize={}&apiKey={}'
+   
     TOP_HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
